e034aaa00310588e4304b430d730a7bd0e379330
Eva <michi991395@gmail.com>
2020-04-27 21:52:38 +0300

    new files

--- z34.py
+++ z34.py
@@ -1,17 +1,28 @@
-f = open("data.txt")
+try:
+    f = open("data.txt")
+except:
+    print("cannot open file")
 k = 0
 n = 0
 s = 0
+fl = 0
 for line in f:
     for i in line.split():
         if k == 0:
             n = i
-            k = k + 1
+            k = 1
+            fl=1
         else:
-            if i < n:
-                k = 1
-            if i == n:
-                k = k + 1
-            n = i
-print(k)
+            if i==n and fl==1:
+                k=k+1
+            if i>n and fl==1:
+                s=k
+                fl=0
+            if i<n:
+                fl=1
+                k=1
+            n=i
+if fl==1:
+    s=k
+print(s)
 f.close()
